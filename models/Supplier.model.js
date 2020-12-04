d6a67b2906fdd576e0afac1801b136f744e4d0e4
Maria Reyna <mereynag@gmail.com>
2020-12-04 18:24:39 -0400

    Avances

---
+++ models/Supplier.model.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose')
+
+const supplierSchema = new Schema(
+    {
+      userID: Schema.Types.ObjectId,
+      orderNum: String,
+      date: String,
+      customer: String,
+      total: String,
+      payment: {
+          type: String,
+          enum: ['UNPAID', 'PAID']
+      },
+      fulfillment: {
+          type: String,
+          enum: ['PENDING', 'FULFILLED', 'CANCELLED']
+      },
+      items: Number,
+      extra: String
+    },
+    {
+      timestamps: true,
+    }
+  );
+  
+  module.exports = model('Order', orderSchema);
+  
