bece6f39d9456f2fe124ce6bfbd8b49352bbb488
Francisco Vivas <franciscovivas.codes@gmail.com>
2020-12-07 21:43:57 -0500

    Add getAvailableProducts Route-Controller

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,66 +1,80 @@
-const Order = require('../models/Order.model')
-const User = require('../models/User.model')
+const Order = require("../models/Order.model");
+const User = require("../models/User.model");
 
 exports.getOrders = async (req, res) => {
-    const { user: { id } } = req
-    const user = await User.findById(id).populate('ordersID')
-    const { ordersID } = user
-    res.status(200).json(ordersID)
-}
+  const {
+    user: { id },
+  } = req;
+  const user = await User.findById(id).populate("ordersID");
+  const { ordersID } = user;
+  res.status(200).json(ordersID);
+};
 
 exports.createOrder = async (req, res) => {
-    const {
-        date,
-        customer,
-        total,
-        payment,
-        fulfillment,
-        items,
-        extra
-    } = req.body
-    const { user: { id, ordersID } } = req
-    const newOrder = await Order.create({
-        userID: id,
-        orderNum: ordersID.length + 1,
-        date,
-        customer,
-        total,
-        payment,
-        fulfillment,
-        items,
-        extra
-    })
-    await User.findByIdAndUpdate(id, { $push: { ordersID: newOrder._id } })
-    res.status(201).json(newOrder)
-}
+  const {
+    date,
+    customer,
+    total,
+    payment,
+    fulfillment,
+    items,
+    extra,
+    itemsQuantity,
+    itemsSalePrice,
+    itemsSubtotal,
+  } = req.body;
+  const {
+    user: { id, ordersID },
+  } = req;
+  const newOrder = await Order.create({
+    userID: id,
+    orderNum: ordersID.length + 1,
+    date,
+    customer,
+    total,
+    payment,
+    fulfillment,
+    items,
+    extra,
+    itemsQuantity,
+    itemsSalePrice,
+    itemsSubtotal,
+  });
+  await User.findByIdAndUpdate(id, { $push: { ordersID: newOrder._id } });
+  res.status(201).json(newOrder);
+};
 
 exports.updateOrder = async (req, res) => {
-    const { id } = req.params
-    const {
-        date,
-        customer,
-        total,
-        payment,
-        fulfillment,
-        items,
-        extra        
-    } = req.body
-    const updatedOrder = await Order.findByIdAndUpdate(id, {
-        date,
-        customer,
-        total,
-        payment,
-        fulfillment,
-        items,
-        extra
-    }, { new: true })
+  const { id } = req.params;
+  const {
+    date,
+    customer,
+    total,
+    payment,
+    fulfillment,
+    items,
+    extra,
+  } = req.body;
+  const updatedOrder = await Order.findByIdAndUpdate(
+    id,
+    {
+      date,
+      customer,
+      total,
+      payment,
+      fulfillment,
+      items,
+      extra,
+    },
+    { new: true }
+  );
 
-        res.status(200).json(updatedOrder)
-}
+  res.status(200).json(updatedOrder);
+};
 
 exports.getOrderDetails = async (req, res) => {
-    const { id } = req.params
-    const order = await Order.findById(id)
+  const { id } = req.params;
+  const order = await Order.findById(id).populate("items");
 
-    res.status(200).json(order)
-}
+  res.status(200).json(order);
+};
